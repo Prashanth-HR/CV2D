c06c29cf494953e76aec7d09ea8bce7aa1dffd85
Carlo Cagnetta <cagnettas@hotmail.it>
2022-02-10 16:35:02 +0100

    Load features settings from config file

--- camera_control.py
+++ camera_control.py
@@ -16,48 +16,13 @@ camera = py.InstantCamera(tlf.CreateFirstDevice())
 camera.Open()
 print("Using device ", camera.GetDeviceInfo().GetModelName())
 
-#######Auto Function ROI #####################################################
-## Enable the 'Brightness' auto function (Gain Auto + Exposure Auto)
-## for the auto function ROI selected
-# camera.AutoFunctionROIUseBrightness.SetValue(True)
-# ## Highlight the auto function ROI selected
-# camera.AutoFunctionROIHighlight.SetValue(True)
-
-####### Auto Function Profile ###################################################
-# ## Set the auto function profile to Exposure Minimum
-# camera.AutoFunctionProfile.SetValue("MinimizeExposureTime");
-# print("camera.AutoFunctionProfile.GetValue(): ", camera.AutoFunctionProfile.GetValue())
-#
-# ## Set the auto function profile to Gain Minimum
-# camera.AutoFunctionProfile.SetValue("MinimizeGain")
-# print("camera.AutoFunctionProfile.GetValue(): ", camera.AutoFunctionProfile.GetValue())
-
-# ## Enable Gain and Exposure Auto auto functions and set the operating mode to Continuous
-# camera.GainAuto.SetValue("Continuous")
-# print("camera.GainAuto.GetValue(): ", camera.GainAuto.GetValue())
-#
-# camera.ExposureAuto.SetValue("Continuous")
-# print("camera.ExposureAuto.GetValue(): ", camera.ExposureAuto.GetValue())
-# camera.ExposureTime.SetValue(30216)
-# camera.AcquisitionFrameRate.SetValue(100)
-# camera.Gamma.SetValue(1)
-# camera.TriggerSelector = "FrameStart"
-# camera.ChunkSelector.SetValue("AutoBrightnessStatus")
-# #################################################################
-# camera.PixelFormat = "Mono8"
-# # like above, alternative is the long form
-# camera.PixelFormat.SetValue("Mono8")
-#
-# # Packet Size
-# camera.GevSCPSPacketSize.SetValue(1500)
-# # Inter-Packet Delay
-# camera.GevSCPD.SetValue(2000)
+nodeFile = "config-a2A3840-13gmPRO_40137700.pfs"
+print("Loading camera configuration from: ", nodeFile)
+py.FeaturePersistence.Load(nodeFile, camera.GetNodeMap(), True)
 
 # # grab one image with a timeout of 1s
 # # returns a GrabResult, which is the image plus metadata
-# res = camera.GrabOne(10000)
-# # the raw memory of the image
-# print(res.GetBuffer()[:100])
+# res = camera.GrabOne(5000)
 # # full method call
 # img = res.GetArray()
 # # abbrev
@@ -68,7 +33,9 @@ print("Using device ", camera.GetDeviceInfo().GetModelName())
 # cv2.imshow("img", img)
 # # need to press a key to get the next image, by the last one the program will exit.
 # cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
+print("Start Grab")
 camera.StartGrabbingMax(100)
 try:
   while camera.IsGrabbing():
