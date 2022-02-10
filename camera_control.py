5d773fd51a49196063ab4384a096d2bd1956be12
Carlo Cagnetta <ge34biy@mytum.de>
2022-02-10 18:07:32 +0100

    new config file,grabbing works in 4k (2160, 3836)

--- camera_control.py
+++ camera_control.py
@@ -1,11 +1,8 @@
+import json
+
 import pypylon.pylon as py
 from PIL import Image
-import pypylon.genicam as geni
-import matplotlib.pyplot as plt
-import numpy as np
 import cv2
-import time
-import pandas as pd
 
 # open the camera
 tlf = py.TlFactory.GetInstance()
@@ -20,37 +17,47 @@ nodeFile = "config-a2A3840-13gmPRO_40137700.pfs"
 print("Loading camera configuration from: ", nodeFile)
 py.FeaturePersistence.Load(nodeFile, camera.GetNodeMap(), True)
 
-# # grab one image with a timeout of 1s
-# # returns a GrabResult, which is the image plus metadata
-# res = camera.GrabOne(5000)
-# # full method call
-# img = res.GetArray()
-# # abbrev
-# img = res.Array
-# # Image.fromarray(res.Array).save("./images.png")
-# print('Dimensions : ', img.shape)
-# cv2.namedWindow("img", cv2.WINDOW_NORMAL)
-# cv2.imshow("img", img)
-# # need to press a key to get the next image, by the last one the program will exit.
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# demonstrate some feature access
+new_width = camera.Width.GetValue() - camera.Width.GetInc()
+if new_width >= camera.Width.GetMin():
+    camera.Width.SetValue(new_width)
 
+# The parameter MaxNumBuffer can be used to control the count of buffers
+# allocated for grabbing. The default value of this parameter is 10.
+camera.MaxNumBuffer = 5
+
+# Grabs 100 times until it gets
 print("Start Grab")
 camera.StartGrabbingMax(100)
 try:
-  while camera.IsGrabbing():
-    result = camera.RetrieveResult(5000, py.TimeoutHandling_ThrowException)
-    print(result.GrabSucceeded())
-    if result.GrabSucceeded():
-      try:
-        # The beef
-        Image.fromarray(result.Array).save("./images/images.png")
-        print("saved image")
-        camera.StopGrabbing()
-      finally:
+    i = 0
+    while camera.IsGrabbing():
+        i = i+1
+        result = camera.RetrieveResult(5000, py.TimeoutHandling_ThrowException)
+        print(i, result.GrabSucceeded())
+        # Image grabbed successfully?
+        if result.GrabSucceeded():
+            # Access the image data.
+            img = result.Array
+            print("Gray value of first pixel: ", img[0, 0])
+            Image.fromarray(result.Array).save("./images/images.png")
+            print("saved image")
+
+            camera.StopGrabbing()
+
+            # Show the image
+            print('Dimensions : ', img.shape)
+            cv2.namedWindow("img", cv2.WINDOW_NORMAL)
+            cv2.imshow("img", img)
+            # need to press a key to get the next image, by the last one the program will exit.
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        else:
+            print("Error: ", result.ErrorCode, result.ErrorDescription)
         result.Release()
+
 finally:
-  camera.StopGrabbing()
-  camera.Close()
+    camera.StopGrabbing()
+    camera.Close()
 
 camera.Close()
